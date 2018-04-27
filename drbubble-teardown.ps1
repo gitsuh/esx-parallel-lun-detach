81c4c52aa2504a8f66ded4a7405d5f3f63934634
gitsuh <21348752+gitsuh@users.noreply.github.com>
2018-04-27 05:03:22 -0400

    initial commit
    must add persistent logging

---
+++ drbubble-teardown.ps1
@@ -0,0 +1,80 @@
+$arrayofstorage = @()
+
+
+Function Unmount-Datastore {
+	[CmdletBinding()]
+	Param (
+		[Parameter(ValueFromPipeline=$true)]
+		$Datastore
+	)
+	Process {
+		if (-not $Datastore) {
+			Write-Host "No Datastore defined as input"
+			Exit
+		}
+		Foreach ($ds in $Datastore) {
+			$hostviewDSDiskName = $ds.ExtensionData.Info.vmfs.extent[0].Diskname
+			if ($ds.ExtensionData.Host) {
+				$attachedHosts = $ds.ExtensionData.Host
+				Foreach ($VMHost in $attachedHosts) {
+					$hostview = Get-View $VMHost.Key
+					$StorageSys = Get-View $HostView.ConfigManager.StorageSystem
+					Write-Host "Unmounting VMFS Datastore $($DS.Name) from host $($hostview.Name)..."
+					$StorageSys.UnmountVmfsVolume($DS.ExtensionData.Info.vmfs.uuid);
+				}
+			}
+		}
+	}
+}
+
+Function Detach-Datastore {
+	[CmdletBinding()]
+	Param (
+		[Parameter(ValueFromPipeline=$true)]
+		$Datastore
+	)
+	Process {
+		if (-not $Datastore) {
+			Write-Host "No Datastore defined as input"
+			Exit
+		}
+		Foreach ($ds in $Datastore) {
+			$hostviewDSDiskName = $ds.ExtensionData.Info.vmfs.extent[0].Diskname
+			if ($ds.ExtensionData.Host) {
+				$attachedHosts = $ds.ExtensionData.Host
+				Foreach ($VMHost in $attachedHosts) {
+					$hostview = Get-View $VMHost.Key
+					$StorageSys = Get-View $HostView.ConfigManager.StorageSystem
+					$devices = $StorageSys.StorageDeviceInfo.ScsiLun
+					Foreach ($device in $devices) {
+						if ($device.canonicalName -eq $hostviewDSDiskName) {
+							$LunUUID = $Device.Uuid
+							Write-Host "Detaching LUN $($Device.CanonicalName) from host $($hostview.Name)..."
+							$StorageSys.DetachScsiLun($LunUUID);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+
+$datastores = Get-Datastore | where {$_.name -like "snap-*"} 
+
+foreach ($item in $datastores){
+	$tempobj = New-Object System.Object
+	$tempobj | add-member -name "Datastore" -membertype NoteProperty -value $item
+	$tempobj | add-member -name "NAAID" -membertype NoteProperty -value $item.ExtensionData.Info.Vmfs.Extent[0].DiskName
+	$arrayofstorage += $tempobj
+}
+
+foreach ($item in $arrayofstorage){
+	write-host $item.Datastore "with naaid" $item.NAAID
+	unmount-datastore -datastore $item.Datastore
+	detach-datastore -datastore $item.Datastore
+}
+
+
+
+
